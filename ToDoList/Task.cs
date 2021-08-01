fa6f6d49512835a9de3087efbef163dc0747d06b
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-08-01 20:50:23 +0200

    Added Day.cs, JSONController.cs, Task.cs

---
+++ ToDoList/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList
+{
+    class Task
+    {
+        private string Name;
+        private string Description;
+        private bool Finished;
+        public Task(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            //Finished = false;
+        }
+
+        public void TaskInfo() { }
+        public void EditTask() { }
+        public void RemoveTask() { }
+    }
+}
